c9982cdf08ac298148478486ee4384331533da84
skssksssksksssss <109153615+skssksssksksssss@users.noreply.github.com>
2022-08-10 22:06:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+94788730196'] //Owner number in aive msg
-global.premium = ['+94788730196'] //Owner Number info
-global.ownernomer = '+94788730196' //Owner Number <<<
+global.owner = ['+94761284534'] //Owner number in aive msg
+global.premium = ['+94761284534'] //Owner Number info
+global.ownernomer = '+94761284534' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
